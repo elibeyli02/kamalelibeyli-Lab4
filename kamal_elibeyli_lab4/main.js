54961c9acc88c52d48dc3eca07e2027c1d2ea7c5
elibeyli02 <127826462+elibeyli02@users.noreply.github.com>
2023-05-09 22:05:42 +0400

    Add files via upload

---
+++ kamal_elibeyli_lab4/main.js
@@ -0,0 +1,26 @@
+let fruit = parseInt(prompt());
+switch (fruit) {
+  case 1:
+    alert('Bazar Ertəsi');
+    break;
+  case 2:
+    alert('Çərşəmbə Axşamı');
+    break;
+  case 3:
+    alert('Çərşəmbə');
+    break;
+  case 4:
+    alert('Cümə Axşamı');
+    break;
+  case 5:
+    alert('Cümə');
+    break;
+  case 6:
+    alert('Şənbə');
+    break;
+  case 7:
+    alert('Bazar');
+    break;
+  default:
+    alert("Gun Daxil edin");
+}
